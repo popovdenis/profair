70cdfbe1ac577d91251cef42ae98a741f39727b9
Denis Popov <denis@balanceinternet.com.au>
2022-09-22 12:45:45 +0200

    Add counter

--- app/code/Profair/RequestPopup/view/frontend/web/js/countdown-widget.js
+++ app/code/Profair/RequestPopup/view/frontend/web/js/countdown-widget.js
@@ -28,9 +28,7 @@ define([
                 dateAndTime: self.options.dateAndTime,
                 size : "lg",
                 regexpMatchFormat: "([0-9])([0-9]):([0-9])([0-9]):([0-9])([0-9]):([0-9])([0-9])",
-// 			regexpReplaceWith: "$1 days $2 hours $3 minutes $4 seconds REMAINING"
                 regexpReplaceWith: countdownRenderer.getCountdownTemplate()
-                //"$1<sup>days</sup> / $2<sup>hours</sup> / $3<sup>minutes</sup> / $4<sup>seconds</sup>"
             });
         }
     });
